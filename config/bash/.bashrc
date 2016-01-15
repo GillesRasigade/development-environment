2b941ad29ecf9b91e7198fb7617acda931d69c6a
Gilles <gilles.rasigade@gmail.com>
2016-01-16 00:46:12 +0100

    Updating bashrc and zshrc configurations to install and use nodejs

--- config/bash/.bashrc
+++ config/bash/.bashrc
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Add following line to /.bashrc to show Git branch name in ssh prompt
-PS1='\[\033[0;31m\]\w\[\033[0;33m\]$(__git_ps1)\[\e[0m\]$ '
+# Using Node 5
+nvm use 5
+
+# Starting zsh
+zsh
--- installation.sh
+++ installation.sh
@@ -14,14 +14,29 @@ function mkdirp() {
   fi
 }
 
+function cpf() {
+  if [ -f "$2" ]; then
+    rm -f "$2";
+  fi
+  cp "$1" "$2"
+}
+
 function addline_once() {
-  echo "$1" "$2"
   r=$( grep "$1" "$2" )
-  echo $r
   if [ "" == "$r" ]; then
     e "Adding line $1 to file $2"
     echo -e "\n$1" >> "$2"
   fi
+}
+
+function install_once() {
+  for package in "$@"; do
+    r=$( dpkg -s "$package" | grep "Package" )
+    if [ "" == "$r" ]; then
+      e "Installing package $package"
+      sudo apt-get -yq install "$package"
+    fi
+  done
 }
 
 USER=$( whoami )
@@ -36,7 +51,7 @@ function init() {
   e "Checking personal configuration"
 
   # Git:
-  if [ ! -e "~/.gitconfig" ]; then
+  if [ ! -f "~/.gitconfig" ]; then
     t "Initializing git"
     rm -f "/home/${USER}/.gitconfig"
     cp "${PWD}/config/git/.gitconfig" "/home/${USER}/.gitconfig"
@@ -50,19 +65,51 @@ function init() {
     ln -fs "${PWD}/config/sublime-text-3/Packages/User" "/home/${USER}/.config/sublime-text-3/Packages/User"
   fi
 
+  # zsh:
+  t "Initializing .zshrc"
+  ln -fs "${PWD}/config/zsh/.zshrc" "/home/${USER}/.zshrc"
+
   # bashrc:
   t "Initializing .bashrc"
   addline_once "source ${PWD}/config/bash/.bashrc" "/home/${USER}/.bashrc"
-  source "/home/${USER}/.bashrc"
+  . "/home/${USER}/.bashrc"
 }
 
 function installation() {
   e "Minimal packages installation"
+
+  # Basic packages installation:
+  install_once openssh-client openssh-server curl wget git zsh
+
+  # Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
+  if [ ! "~/.oh-my-zsh" ]; then
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  fi
+
+  # NodeJS minimal:
+  install_once nodejs npm
+
+  # NVM most updated version
+  if [ ! -d "/home/${USER}/.nvm" ]; then
+    e "NVM installation"
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
+    . ~/.bashrc
+  fi
+
+  # Adding nvm
+  . "$NVM_DIR/nvm.sh"
+
+  # Installing Node 5
+  nvm install 5
+
+  # Installing development global dependencies
+  npm install -g grunt-cli bower mocha mocha-co istanbul node-inspector
+
 }
 
 t "INSTALLATION"
 system
-init
 installation
+init
 
 exit 0
